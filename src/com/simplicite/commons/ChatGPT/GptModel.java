b8193ac4e783a80a180ee8d6b5302874599355a9
designer (Designer) <>
2024-02-01 10:42:27 +0100

    better int and decimal generation

--- src/com/simplicite/commons/ChatGPT/GptModel.java
+++ src/com/simplicite/commons/ChatGPT/GptModel.java
@@ -459,8 +459,10 @@ public class GptModel implements java.io.Serializable {
 			fld.setFieldValue("fld_fonctid", isKey);
 			fld.setFieldValue("fld_required", isRequired);
 			fld.setFieldValue(MODULE_ID_FIELD,	mInfo.moduleId);
-			if(type == ObjectField.TYPE_FLOAT || type == ObjectField.TYPE_BIGDECIMAL || type == ObjectField.TYPE_INT){
+			if(type == ObjectField.TYPE_FLOAT || type == ObjectField.TYPE_BIGDECIMAL ){
 				fld.setFieldValue("fld_precision", 2);
+				fld.setFieldValue("fld_size", 5);
+			}else if(type == ObjectField.TYPE_INT){
 				fld.setFieldValue("fld_size", 5);
 			}else{
 				fld.setFieldValue("fld_size", 100);
